d6fa9ac0f07e0cf95cc6c96a8f45492b13e554d6
EthanDotLog <148747739+EthanDotLog@users.noreply.github.com>
2024-02-24 20:13:47 -0600

    update
    mobile and desktop friendly

--- scripts/script.js
+++ scripts/script.js
@@ -14,34 +14,50 @@ const userValues = {
 
 for (let i = 0; i < buttonsArray.length; i++) {
   if (buttonsArray[i].value === "backspace") {
-    buttonsArray[i].addEventListener("click", ()=>{
+    buttonsArray[i].addEventListener("click", (e)=>{
       if (userValues.input.length > 0) {
         userValues.input.pop();
-        printOut(i)
+        printOut(e)
       } else {
         results.textContent = "nom nom nom"
       }
     })
     } else if (buttonsArray[i].value === "clear") {
-      buttonsArray[i].addEventListener("click", ()=> {
+      buttonsArray[i].addEventListener("click", (e)=> {
         userValues.input = []
-        userValues.previous = 0
-        printOut(i)
+        printOut(e)
+      })
+    } else if (buttonsArray[i].value === "=") {
+      buttonsArray[i].addEventListener("click", (e)=> {
+        printOut(e)
+        userValues.input = []
       })
     } else {
-
-    buttonsArray[i].addEventListener("click", ()=>{
+    buttonsArray[i].addEventListener("click", (e)=>{
       userValues.input.push(buttonsArray[i].value);
-      printOut(i)
+      printOut(e)
     })
   }
 }
 
 
 const printOut = (i) => {
-  results.textContent = userValues.input.join("")
-  if (!["+", "-", "*", "/", "%", "(", "clear"].includes(buttonsArray[i].value)) {
-    running.textContent = userValues.evaluation()
+  const callButton = i.target.value
+  console.log(callButton, callButton.value)
+  if (callButton === "clear") {
+    results.textContent = "";
+    running.textContent = "";
+  } else if (callButton === "backspace") {
+    results.textContent = userValues.input.join("");
+    running.textContent = userValues.evaluation();
+  } else if (callButton === "=" ) {
+    results.textContent = userValues.evaluation();
+    running.textContent = ""
+  } else if (!["+", "-", "*", "/", "%", "("].includes(callButton) && userValues.input.length > 3) {
+    results.textContent = userValues.input.join("")
+    running.textContent =  userValues.evaluation()
+  } else {
+    results.textContent = userValues.input.join("")
   }
 
   
